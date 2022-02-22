9884f4f7f2829f394716e40a28f0c86e53946bda
Jeve-Stobs <samuelacasey@gmail.com>
2022-02-21 22:35:39 -0500

    fix

--- api/server.js
+++
@@ -1,9 +0,0 @@
-const cron = require('node-cron')
-const shell = require('shelljs')
-cron.schedule('*/10 * * * * *', function () {
-	if (shell.exec('node index.js').code !== 0) {
-		shell.exit(1)
-	} else {
-		shell.echo('Database backup complete')
-	}
-})
