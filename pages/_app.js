1ea9528716735c670752486c85b0524fda303ca2
Jeve-Stobs <samuelacasey@gmail.com>
2022-02-22 01:16:46 -0500

    favicon isn't working

--- pages/_app.js
+++ pages/_app.js
@@ -21,14 +21,14 @@ function App({ Component, pageProps }) {
 					crossOrigin="anonymous"
 				/>
 				<meta name="description" content="I'll put something here laters" />
-				<link rel="icon" href="/favicon.ico" />
+				<link rel="icon" href="https://partysover.vercel.app/favicon.ico" />
 				<meta
 					property="og:title"
 					content="Are we fukt? | An of analysis of recessionary indicators"
 				/>
 				<meta property="og:locale" content="en_US" />
 				<meta property="og:url" content="https://jevestobs.dev" />
-				<meta property="og:image" content="/favicon.ico" />
+				<meta property="og:image" content="https://cms.qz.com/wp-content/uploads/2018/07/Jerome_Powell-Face.jpg" />
 				<meta property="og:image:width" content="120" />
 				<meta property="og:image:height" content="108" />
 				<meta property="og:type" content="website" />
@@ -45,7 +45,7 @@ function App({ Component, pageProps }) {
 					name="twitter:description"
 					content="I'll put something here laters"
 				/>
-				<meta name="twitter:image" content="/favicon.ico" />
+				<meta name="twitter:image" content="https://cms.qz.com/wp-content/uploads/2018/07/Jerome_Powell-Face.jpg" />
 				<meta
 					name="Keywords"
 					content="finance, fed, jerome powell, recession, financial analysis, indicators, charts, graphs, yield curve"
