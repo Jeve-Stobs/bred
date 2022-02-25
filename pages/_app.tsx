c0b5af9382555f373690c8e8f058345af133ca83
Jeve-Stobs <samuelacasey@gmail.com>
2022-02-25 18:24:45 -0500

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App(props: AppProps) {
 
   return (
     <>
-      <Head>
+      		<Head>
 				<meta
 					name="viewport"
 					content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
