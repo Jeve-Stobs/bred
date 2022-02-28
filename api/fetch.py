5dd513d175659018afafb9cb203e03c2c89957b8
Jeve-Stobs <samuelacasey@gmail.com>
2022-02-27 21:16:49 -0500

    ✨fetch new data every three minutes

--- api/fetch.py
+++ api/fetch.py
@@ -1,5 +1,7 @@
 import os
 import json
+import time
+import schedule
 from fredapi import Fred
 from dotenv import load_dotenv
 from datetime import datetime
@@ -11,26 +13,26 @@ load_dotenv()
 # set date values
 one_year_ago = (datetime.today() - relativedelta(months=13)).strftime('%Y/%m/%d')
 today = datetime.today().strftime('%Y/%m/%d')
+def func():
+    # set fred api key and initialize client
+    fred = Fred(api_key=os.getenv('FRED_API_KEY'))
+    unrate = fred.get_series('UNRATE', observation_start=one_year_ago, observation_end=today)
+    u6rate = fred.get_series('U6RATE', observation_start=one_year_ago, observation_end=today)
+    cpi = fred.get_series('CPIAUCSL', observation_start=one_year_ago, observation_end=today)
+    rates = fred.get_series('DFII10', observation_start=one_year_ago, observation_end=today)
+    policy = fred.get_series('USEPUINDXD', observation_start=one_year_ago, observation_end=today)
+    alcohol = fred.get_series('SMU06000004244530001', observation_start=one_year_ago, observation_end=today)
+    initial_claims = fred.get_series('ICSA', observation_start=one_year_ago, observation_end=today)
+    total_nonfarm_payroll = fred.get_series('PAYEMS', observation_start=one_year_ago, observation_end=today)
+    deflation_probability = fred.get_series('STLPPMDEF', observation_start=one_year_ago, observation_end=today)
+    key_inflation = fred.get_series('PCEPI', observation_start=one_year_ago, observation_end=today)
 
-# set fred api key and initialize client
-fred = Fred(api_key=os.getenv('FRED_API_KEY'))
-unrate = fred.get_series('UNRATE', observation_start=one_year_ago, observation_end=today)
-u6rate = fred.get_series('U6RATE', observation_start=one_year_ago, observation_end=today)
-cpi = fred.get_series('CPIAUCSL', observation_start=one_year_ago, observation_end=today)
-rates = fred.get_series('DFII10', observation_start=one_year_ago, observation_end=today)
-policy = fred.get_series('USEPUINDXD', observation_start=one_year_ago, observation_end=today)
-alcohol = fred.get_series('SMU06000004244530001', observation_start=one_year_ago, observation_end=today)
-initial_claims = fred.get_series('ICSA', observation_start=one_year_ago, observation_end=today)
-total_nonfarm_payroll = fred.get_series('PAYEMS', observation_start=one_year_ago, observation_end=today)
-deflation_probability = fred.get_series('STLPPMDEF', observation_start=one_year_ago, observation_end=today)
-key_inflation = fred.get_series('PCEPI', observation_start=one_year_ago, observation_end=today)
+    # fetch tradingview data for bond yields
+    analysis = get_multiple_analysis(screener="cfd", interval=Interval.INTERVAL_1_DAY, symbols=["TVC:US10Y", "TVC:US02Y"])
+    us10y_analysis = list(analysis.values())[0]
+    us02y_analysis = list(analysis.values())[-1]
 
-# fetch tradingview data for bond yields
-analysis = get_multiple_analysis(screener="cfd", interval=Interval.INTERVAL_1_DAY, symbols=["TVC:US10Y", "TVC:US02Y"])
-us10y_analysis = list(analysis.values())[0]
-us02y_analysis = list(analysis.values())[-1]
-
-index = {
+    index = {
             'US10Y' : {
                 'value' : us10y_analysis.indicators["close"],
                 'previous' : us10y_analysis.indicators["open"]
@@ -81,7 +83,13 @@ index = {
                 'old' : key_inflation.iloc[0],
                 'lastMonth' : key_inflation.iloc[-2]
             },
-}
-# write to json file
-with open('data.json', 'w') as outfile:
-    json.dump(index, outfile)
+    }
+    # write to json file
+    with open('data.json', 'w') as outfile:
+        json.dump(index, outfile)
+        print('wrote to data.json')
+schedule.every(5).minutes.do(func)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
