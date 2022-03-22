d9b251041c64c9df6efdf9c6e8d06a7ab4105703
Jeve-Stobs <samuelacasey@gmail.com>
2022-03-21 22:16:59 -0400

    💄 don't preload fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,20 +25,6 @@ export default function App(props: AppProps) {
 					as="fetch"
 					crossOrigin="anonymous"
 				/>
-				<link
-					rel="preload"
-					href="/font/8b42e3fc6d1d161d6fbd7487babe6cfe.woff2"
-					as="font"
-					type="font/woff2"
-					crossOrigin="anonymous"
-				/>
-				<link
-					rel="preload"
-					href="/font/f31b2ecb2f8e039d53bd75d5314229c7.woff2"
-					as="font"
-					type="font/woff2"
-					crossOrigin="anonymous"
-				/>
 				<meta
 					name="description"
 					content="A data aggregation tool built to gauge the health of financial markets"
