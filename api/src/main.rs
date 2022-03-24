25f86f49b78ae6b762ac798a5edeb914e28780f3
Jeve-Stobs <samuelacasey@gmail.com>
2022-03-23 22:02:40 -0400

    ⚗️ increase fetch rate

--- api/src/main.rs
+++ api/src/main.rs
@@ -41,7 +41,7 @@ async fn index() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     thread::spawn(move || loop {
         write_to_file();
-        thread::sleep(Duration::from_secs(180));
+        thread::sleep(Duration::from_secs(30));
     });
     // initialize tracing
     let subscriber = Registry::default()
